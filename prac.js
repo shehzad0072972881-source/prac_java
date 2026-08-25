52c1a0d440c4e1301775c7bcc664a50dfb62bfda
shehzad0072972881-source <shehzad0072972881@gmail.com>
2026-08-25 19:31:37 +0500

    create tic toc toe game

--- prac.js
+++ prac.js
@@ -32,15 +32,66 @@
 // let heading = document.querySelector("h1")
 // let heading2 = document.querySelector("h2")
 
-//    PRACTICE
-// let h2 = document.querySelector("h2")
-// console.dir(h2.innerText);
+// let box = document.querySelector(".box")
+// box.onclick = (evt)=> {
+//     console.log("div par click karne se div ka color bi change hoga or text color bi.");
+//     box.style.background = "linear-gradient(to right, red, yellow, blue)"
+//     box.style.color = "white";
+//     console.log(evt)
+// };
 
-// h2.innerText = h2.innerText + "from apna college student"
 
+// let heading = document.querySelector("#head2");
+// head2.ondblclick = (event)=> {
+//     console.log("double click karne par iska color change hoga or ye bg color bi change hoga.");
+//     head2.style.background = "linear-gradient(to right, red, blue, yellow, grey)";
+//     head2.style.color = "white";
+//     head2.style.fontStyle = "italic";
+//     head2.style.display = "flex";
+//     head2.style.justifyContent = "center";
+//     head2.style.alighnItems = "center";
+// };
 
-//    practice question 2
-// new code
+// let btn = document.querySelector("#btn1")
+// btn1.onmouseover = (e)=> {
+//     console.log("btn par over karne par color change hoga or border radius bi change hoga or tora sa bara hogaege btn.");
+//     console.log(e.target);
+//      console.log(e.type);
+//      console.log(e.clientX, e.clientY)
+//     btn1.style.background = "linear-gradient(to right, red, yellow)";
+//     btn1.style.color = "white";
+//     btn1.style.borderRadius = "20px";
+//     btn1.style.fontSize = "20px"
+//     console.log(btn1)
+// }
 
-let divs = document.querySelectorAll(".box");
-divs[0].innerText = "new unique value 1";
+// let btn1 = document.querySelector("#btn1")
+// function btnClick(e) {
+//     console.log("btn par click hua");
+//     btn1.style.background = "linear-gradient(to right, red, yellow)";
+//     btn1.style.color = "white";
+//     btn1.style.borderRadius = "20px";
+//     btn1.style.fontSize = "20px";
+//     console.log(e)
+// }
+// btn1.addEventListener("click", btnClick);
+
+// let btn2 = document.querySelector("#btn2");
+// let body = document.querySelector("body");
+// let currMode = "light";
+
+// btn2.addEventListener("click", () =>{
+//     if (currMode === "light") {
+//         currMode = "dark";
+//         body.classList.add("dark");
+//            body.classList.remove("light")
+//     }else {
+//         currMode = "light"
+//         body.classList.add("light")
+//         body.classList.remove("dark");
+//     }
+//     console.log(currMode);
+// });
+
+//       GAME TIC TOC TOE
+
