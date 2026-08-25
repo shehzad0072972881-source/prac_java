acc7841c265a5bfea89d6e9ede4b424c36883e11
shehzad0072972881-source <shehzad0072972881@gmail.com>
2026-08-25 11:58:09 +0500

    init commit

---
+++ prac.js
@@ -0,0 +1,44 @@
+// let headings = document.getElementById("headings")
+// console.log(headings);
+// let allimages = document.getElementById("allimages")
+// console.log(allimages);
+// let head1 = document.getElementsByClassName("head1")
+// console.log(head1);
+// let para = document.getElementsByTagName("p")
+// console.log(para);
+
+// let firstEl = document.querySelector("p")
+// console.log(firstEl);
+
+// //      property
+// let firstEleme = document.querySelector(".head1")
+// firstEleme.innerHTML = "Shehzad khan";
+
+// // let allEl = document.querySelectorAll("p")
+// // console.log(allEl);
+
+// let allEle = document.querySelectorAll("#allimages")
+// console.log(allEle);
+
+// let allElem = document.querySelectorAll("#images")
+// console.log(allElem);
+
+
+//            FRUITS
+
+// let div = document.querySelector("div")
+// console.log(div);
+
+// let heading = document.querySelector("h1")
+// let heading2 = document.querySelector("h2")
+
+//    PRACTICE
+// let h2 = document.querySelector("h2")
+// console.dir(h2.innerText);
+
+// h2.innerText = h2.innerText + "from apna college student"
+
+//    practice question 2
+
+let divs = document.querySelectorAll(".box");
+divs[0].innerText = "new unique value 1";
